99e27b13038af79014ff1d8649bed84310d15cf2
Saifudheenpv <mesaifudheenpv@gmail.com>
2025-05-15 07:55:51 +0530

    Day 3: Created UserModel and WelcomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'features/welcome/welcome_screen.dart'; // 👈 Add this import
 
 void main() {
-  runApp(const LifeMateApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'LifeMate',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const WelcomeScreen(), // 👈 Launch WelcomeScreen directly
+    );
+  }
 }
